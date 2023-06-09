74d8a3c1abd691050f5298f5cb8ce59d7b56129c
westontnelson@outlook.com <westontnelson@outlook.com>
2023-06-09 02:52:56 -0500

    Update Base App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,42 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+      {/* Top Section */}
+      <h1 className={styles.h1}>Based and Non-Fungible Free Mint!</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/mint`)}
+        >
+          {/* Mint a new NFT */}
+          <Image src="/icons/based.png" alt="token" width={256} height={256} />
+          <h2 className={styles.selectBoxTitle}>Need to try out using Base? Here you go!</h2>
+          <p className={styles.selectBoxDescription}>
+            Mint a collectible here. Interacting on the Base testnet is a great way to show you are an early adopter. 
+          </p>
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <Image src="/icons/token2.svg" alt="token" width={256} height={256} />
+          <h2 className={styles.selectBoxTitle}>Learn about Base</h2>
+          <p className={styles.selectBoxDescription}>
+            Base is poised to onboard 1 billion users to web3
+          </p>
+          
         </div>
-      </main>
+      </div>
     </div>
   );
 };
